a05605a9b6151e50d096e1a9ab30b08ac54e52dd
William <2246747@cstj.qc.ca>
2025-10-09 16:54:23 -0400

    Implemented User populate based on role for Event

--- src/repositories/event.repository.js
+++ src/repositories/event.repository.js
@@ -1,13 +1,15 @@
-import Event from '../models/Event.js';
-import Category from '../models/Category.js';
+import Event from "../models/Event.js";
+import Category from "../models/Category.js";
+import User from "../models/User.js";
+import Client from "../models/Client.js";
+import Organisation from "../models/Organisation.js";
 
 class EventRepository {
   async create(eventData) {
-    try 
-    {
+    try {
       return await Event.create(eventData);
     } catch (error) {
-        throw error;
+      throw error;
     }
   }
 
@@ -20,45 +22,57 @@ class EventRepository {
       include: [
         {
           model: Category,
-          attributes: ['Name']
-        }
-      ]
+          attributes: ["Name"],
+        },
+        {
+          model: User,
+          attributes: ["ID"],
+          include: [
+            {
+              model: Client,
+              attributes: ["FirstName", "LastName"],
+              required: false,
+            },
+            {
+              model: Organisation,
+              attributes: ["Name"],
+              required: false,
+            },
+          ],
+        },
+      ],
     });
   }
 
   async update(id, eventData) {
     const [updatedRowsCount] = await Event.update(eventData, {
-      where: { ID: id }
+      where: { ID: id },
     });
     return updatedRowsCount > 0;
   }
 
   async delete(id) {
     try {
-
       const deletedRowsCount = await Event.destroy({
-      where: { ID: id }
-    });
-    return deletedRowsCount > 0;
-      
+        where: { ID: id },
+      });
+      return deletedRowsCount > 0;
     } catch (error) {
       throw error;
     }
-
   }
 
-  // Retrouve tous les évenements associés à un 
+  // Retrouve tous les évenements associés à un
   async findByUserId(userId) {
     return await Event.findAll({
-      where: { UserID: userId }
+      where: { UserID: userId },
     });
   }
-
 
   // Retourne tous les évenements qui ont étés approuvés
   async findByApprovalStatus(approved) {
     return await Event.findAll({
-      where: { Approved: approved }
+      where: { Approved: approved },
     });
   }
 
@@ -68,6 +82,16 @@ class EventRepository {
       event.Category = event.Category.Name;
     }
     delete event.CategoryID;
+
+    // Transform User to Creator field
+    if (event.User) {
+      if (event.User.Organisation) {
+        event.Creator = event.User.Organisation.Name;
+      } else if (event.User.Client) {
+        event.Creator = `${event.User.Client.FirstName} ${event.User.Client.LastName}`;
+      }
+      delete event.User;
+    }
 
     return event;
   }
