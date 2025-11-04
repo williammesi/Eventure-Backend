bb39c5ab199a090b47ad53aab424be9ea3f072dc
6223355 <6223355@cstj.qc.ca>
2025-11-04 15:08:42 -0500

    ahh

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -92,51 +92,7 @@ class UserRepository {
     async validateRefreshToken(email, headerBase64) {
         //TODO:
     }
-    async update(id, data) {
-        try {
-            const { username, email, organisationName, phoneNumber } = data;
-            
-            console.log("Mise à jour utilisateur:", id, data);
 
-            // Récupérer l'utilisateur avec son organisation
-            const user = await User.findByPk(id, {
-                include: [{
-                    model: Organisation,
-                    as: 'Organisation'
-                }]
-            });
-
-            if (!user) {
-                throw HttpErrors.NotFound('Utilisateur non trouvé');
-            }
-
-            // Mettre à jour les champs de l'utilisateur
-            if (username) user.Username = username;
-            if (email) user.Email = email;
-            
-            await user.save();
-
-            // Mettre à jour l'organisation si elle existe
-            if (user.Organisation && (organisationName || phoneNumber)) {
-                if (organisationName) user.Organisation.Name = organisationName;
-                if (phoneNumber) user.Organisation.PhoneNumber = phoneNumber;
-                await user.Organisation.save();
-            }
-
-            // Recharger avec les relations pour retourner les données complètes
-            await user.reload({
-                include: [{
-                    model: Organisation,
-                    as: 'Organisation'
-                }]
-            });
-
-            return user;
-        } catch (err) {
-            console.error("Erreur dans update repository:", err);
-            throw err;
-        }
-    }
     
 
     transform(account) {
