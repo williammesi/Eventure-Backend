28b81819c41aa0b77491550447af73f6518933ba
FelixD <1338749@cstj.qc.ca>
2025-11-19 21:01:17 -0500

    Supprimer commentaire test

--- src/routes/commentaires.routes.js
+++ src/routes/commentaires.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import HttpErrors from "http-errors";
 import commentaireRepository from "../repositories/commentaire.repository.js";
 import { guardAuthorizationJWT } from "../middlewares/authorization.jwt.js";
 
@@ -6,6 +7,7 @@ const router = express.Router();
 
 router.get("/:eventId", retrieveAllForEvent);
 router.post("/", guardAuthorizationJWT, createCommentaire);
+router.delete("/:commentaireId", guardAuthorizationJWT, deleteCommentaire);
 
 async function createCommentaire(req, res, next) {
   try {
@@ -32,6 +34,34 @@ async function retrieveAllForEvent(req, res, next) {
     res.status(200).json(commentaires);
   } catch (err) {
     console.log(err);
+    return next(err);
+  }
+}
+
+async function deleteCommentaire(req, res, next) {
+  try {
+    const commentaireId = req.params.commentaireId;
+    const userId = parseInt(req.auth.userId);
+    const userRoleId = parseInt(req.auth.roleId);
+
+    const commentaire = await commentaireRepository.findById(commentaireId);
+
+    if (!commentaire) {
+      throw HttpErrors.NotFound("Commentaire non trouvé");
+    }
+
+    const isModerator = userRoleId === 3;
+    const isOwner = commentaire.UserID === userId;
+
+    if (!isModerator && !isOwner) {
+      throw HttpErrors.Forbidden(
+        "Vous n'êtes pas autorisé à supprimer ce commentaire"
+      );
+    }
+
+    await commentaireRepository.delete(commentaireId);
+    res.status(204).end();
+  } catch (err) {
     return next(err);
   }
 }
