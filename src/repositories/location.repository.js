b56bb82c354d3df1c53265e903a2f63c8c1830c6
William Mesiti <2246747@cstj.qc.ca>
2025-11-29 20:50:06 -0500

    addresse update fix
    There was a typo

--- src/repositories/location.repository.js
+++ src/repositories/location.repository.js
@@ -6,13 +6,13 @@ class LocationRepository {
     try {
       // Fetch coordinates if address information is provided
       if (
-        locationData.Adress &&
+        locationData.Address &&
         locationData.City &&
         locationData.Province &&
         locationData.Country
       ) {
         const coordinates = await geocodingService.getCoordinates(
-          locationData.Adress,
+          locationData.Address,
           locationData.City,
           locationData.Province,
           locationData.Country
@@ -37,7 +37,7 @@ class LocationRepository {
     try {
       // Fetch new coordinates if address information is being updated
       if (
-        locationData.Adress ||
+        locationData.Address ||
         locationData.City ||
         locationData.Province ||
         locationData.Country
@@ -45,7 +45,7 @@ class LocationRepository {
         const location = await this.findById(id);
         if (location) {
           const coordinates = await geocodingService.getCoordinates(
-            locationData.Adress || location.Adress,
+            locationData.Address || location.Address,
             locationData.City || location.City,
             locationData.Province || location.Province,
             locationData.Country || location.Country
