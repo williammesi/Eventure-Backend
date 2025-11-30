11a29ee8a4f85585e92508abb74fd94f37318085
FelixD <1338749@cstj.qc.ca>
2025-11-30 10:51:20 -0500

    Route delete report

--- src/routes/reports.routes.js
+++ src/routes/reports.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import HttpErrors from "http-errors";
 import reportRepository from "../repositories/report.repository.js";
 import { guardAuthorizationJWT } from "../middlewares/authorization.jwt.js";
 
@@ -6,6 +7,7 @@ const router = express.Router();
 
 router.get("/", guardAuthorizationJWT, retrieveAll);
 router.post("/", guardAuthorizationJWT, create);
+router.delete("/:reportId", guardAuthorizationJWT, deleteReport);
 
 async function retrieveAll(req, res, next) {
   try {
@@ -24,6 +26,22 @@ async function create(req, res, next) {
     res.status(201).json(newReport);
   } catch (err) {
     console.log(err);
+    return next(err);
+  }
+}
+
+async function deleteReport(req, res, next) {
+  try {
+    const reportId = req.params.reportId;
+
+    const deleted = await reportRepository.delete(reportId);
+    
+    if (!deleted) {
+      throw HttpErrors.NotFound("Report non trouvé");
+    }
+    
+    res.status(204).end();
+  } catch (err) {
     return next(err);
   }
 }
