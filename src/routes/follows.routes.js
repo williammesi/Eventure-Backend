98f905ee6c4adddad672e792097561e2720991ce
Hadrien Breton <brehad@hotmail.com>
2025-12-02 17:22:32 -0500

    Bug fix pour le Follow

--- src/routes/follows.routes.js
+++ src/routes/follows.routes.js
@@ -5,24 +5,46 @@ import validator from "../middlewares/validator.js";
 
 import followRepository from "../repositories/follow.repository.js";
 
-import { authenticateToken } from "../middlewares/authorization.jwt.js";
-import eventsValidator from "../validators/events.validator.js";
+import { guardAuthorizationJWT } from "../middlewares/authorization.jwt.js";
 
 const router = express.Router();
 
-router.post("/:type/:id", create);
+router.post("/:type/:id", guardAuthorizationJWT, create);
+router.delete("/:type/:id", guardAuthorizationJWT, remove);
+router.get("/", guardAuthorizationJWT, fetchByUserID);
+
+async function fetchByUserID(req, res, next) {
+  try {
+    const userID = req.auth.userId;
+
+    const follows = await followRepository.retrieveByUserID(userID);
+
+    res.status(200).json(follows);
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+}
 
 async function create(req, res, next) {
   try {
-    if (req.query._body === "false") {
-      return res.status(204).end();
-    }
-
     const { type, id } = req.params;
 
-    followRepository.create(type, id, req.body.userID);
+    await followRepository.create(type, id, req.auth.userId);
 
-    res.status(201);
+    res.status(201).end();
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function remove(req, res, next) {
+  try {
+    const { type, id } = req.params;
+
+    await followRepository.delete(type, id, req.auth.userId);
+
+    res.status(200).end();
   } catch (error) {
     return next(error);
   }
